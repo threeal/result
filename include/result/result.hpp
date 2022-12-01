82b370cfe3799347267ff90728b4b2a55ac1d1f1
Alfi Maulana <alfi.maulana.f@gmail.com>
2022-12-01 21:06:23 +0700

    modify member naming

--- include/result/result.hpp
+++ include/result/result.hpp
@@ -9,19 +9,19 @@ namespace res {
 
 class Result {
  private:
-  std::optional<std::string> error;
+  std::optional<std::string> opt_err_msg;
  public:
   Result(const internal::Ok& ok) {}
 
   template<typename U>
-  Result(const U& err) : error(err) {}
+  Result(const U& err_msg) : opt_err_msg(err_msg) {}
 
-  bool is_ok() const { return !error.has_value(); }
-  bool is_err() const { return error.has_value(); }
+  bool is_ok() const { return !opt_err_msg.has_value(); }
+  bool is_err() const { return opt_err_msg.has_value(); }
 
   std::string unwrap_err() const {
-    if (is_ok()) throw std::runtime_error("is ok");
-    return error.value();
+    if (!opt_err_msg.has_value()) throw std::runtime_error("is ok");
+    return opt_err_msg.value();
   }
 };
 }
