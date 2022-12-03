43923937ab4e1b7ed84261c6c0c4318fcaedd678
Alfi Maulana <alfi.maulana.f@gmail.com>
2022-12-03 21:07:34 +0700

    move ok and err from `res::internal` to `internal`

--- include/result/result.hpp
+++ include/result/result.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "internal/err.hpp"
-#include "internal/ok.hpp"
+#include "err.hpp"
+#include "ok.hpp"
 #include <exception>
 #include <optional>
 
@@ -9,16 +9,16 @@ namespace res {
 
 class Result {
  private:
-  std::optional<internal::Err> err_opt;
+  std::optional<Err> err_opt;
  public:
-  Result() : err_opt(internal::uninitialized_err) {}
-  Result(const internal::Ok& ok) {}
-  Result(const internal::Err& err) : err_opt(err) {}
+  Result() : err_opt(Err("result is uninitialized")) {}
+  Result(const Ok& ok) {}
+  Result(const Err& err) : err_opt(err) {}
 
   bool is_ok() const { return !err_opt.has_value(); }
   bool is_err() const { return err_opt.has_value(); }
 
-  internal::Err unwrap_err() const {
+  Err unwrap_err() const {
     if (!err_opt.has_value()) throw std::runtime_error("is ok");
     return err_opt.value();
   }  // LCOV_EXCL_LINE
--- test/result_test.cpp
+++ test/result_test.cpp
@@ -3,13 +3,13 @@
 #include <string>
 
 TEST_CASE("check ok result") {
-  const res::Result res = res::ok;
+  const res::Result res = res::Ok();
   REQUIRE(res.is_ok());
   REQUIRE_FALSE(res.is_err());
 }
 
 TEST_CASE("check error result") {
-  const res::Result res = res::err("unknown error");
+  const res::Result res = res::Err("unknown error");
   REQUIRE(res.is_err());
   REQUIRE_FALSE(res.is_ok());
 }
@@ -21,36 +21,36 @@ TEST_CASE("uninitialized result contains error") {
 
 TEST_CASE("call `unwrap_err` on error result") {
   const std::string err_msg = "unknown error";
-  const res::Result res = res::err(err_msg);
+  const res::Result res = res::Err(err_msg);
   REQUIRE(res.is_err());
   REQUIRE(res.unwrap_err() == err_msg);
 }
 
 TEST_CASE("call `unwrap_err` on ok result") {
-  const res::Result res = res::ok;
+  const res::Result res = res::Ok();
   REQUIRE(res.is_ok());
   REQUIRE_THROWS(res.unwrap_err());
 }
 
 TEST_CASE("check rewriting result") {
-  res::Result res = res::ok;
+  res::Result res = res::Ok();
   REQUIRE(res.is_ok());
   std::string err_msg = "unknown error";
-  res = res::err(err_msg);
+  res = res::Err(err_msg);
   REQUIRE(res.is_err());
   REQUIRE(res.unwrap_err() == err_msg);
   err_msg = "other error";
-  res = res::err(err_msg);
+  res = res::Err(err_msg);
   REQUIRE(res.is_err());
   REQUIRE(res.unwrap_err() == err_msg);
-  res = res::ok;
+  res = res::Ok();
   REQUIRE(res.is_ok());
 }
 
 namespace {
 res::Result foo(bool is_ok) {
-  if (is_ok) return res::ok;
-  return res::err("unknown error");
+  if (is_ok) return res::Ok();
+  return res::Err("unknown error");
 }
 }
 
