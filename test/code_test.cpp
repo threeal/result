64ef2d1b9d7110a49a7e9017dc5076221b435314
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-18 19:33:27 +0700

    group `test code snippet` by structs

--- test/code_test.cpp
+++ test/code_test.cpp
@@ -7,21 +7,25 @@
 #define assert REQUIRE
 
 TEST_CASE("test code snippet") {
-  SECTION("Ok::Ok()") {
-    res::Result result = res::Ok();
-    assert(result.is_ok());
+  SECTION("res::Ok struct") {
+    SECTION("Ok() constructor") {
+      res::Result result = res::Ok();
+      assert(result.is_ok());
+    }
   }
-  SECTION("Err::Err(const std::string&)") {
-    std::string err_msg = "undefined error";
-    res::Result result = res::Err(err_msg);
-    assert(result.is_err());
-  }
-  SECTION("Err::Err(const char*)") {
-    res::Result result = res::Err("undefined error");
-    assert(result.is_err());
-  }
-  SECTION("Err::Err(const ErrStream&)") {
-    res::Err err = res::ErrStream() << "not found " << 404;
-    assert(err == "not found 404");
+  SECTION("res::Err struct") {
+    SECTION("Err(const std::string&) constructor") {
+      std::string err_msg = "undefined error";
+      res::Result result = res::Err(err_msg);
+      assert(result.is_err());
+    }
+    SECTION("Err(const char*) constructor") {
+      res::Result result = res::Err("undefined error");
+      assert(result.is_err());
+    }
+    SECTION("Err(const ErrStream&) constructor") {
+      res::Err err = res::ErrStream() << "not found " << 404;
+      assert(err == "not found 404");
+    }
   }
 }
