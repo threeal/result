0c7aecd2c452d28579fc50970c292040ccea5c68
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-18 20:10:47 +0700

    remove `Ok::Ok()`

--- include/result/ok.hpp
+++ include/result/ok.hpp
@@ -3,15 +3,6 @@
 namespace res {
 
 /** A type that represents an ok status (success). */
-struct Ok {
-  /** Construct a new ok data.
-   *
-   * @code{.cpp}
-   * res::Result result = res::Ok();
-   * assert(result.is_ok());  // OK
-   * @endcode
-   */
-  Ok() {}
-};
+struct Ok {};
 
 }  // namespace res
