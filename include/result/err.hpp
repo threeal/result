f64661fdacb4c5c1128bb56c113dc4ce0d154097
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-18 18:05:18 +0700

    fill API docs and test for `Err::Err(const std::string&)`

--- include/result/err.hpp
+++ include/result/err.hpp
@@ -7,8 +7,19 @@ namespace res {
 
 struct ErrStream : public std::stringstream {};
 
+/** A type that represents an error status (failure). */
 struct Err : public std::string {
+  /** Construct a new error data using a C++ string.
+   * @param err_msg Error message in C++ string.
+   *
+   * @code{.cpp}
+   * std::string err_msg = "undefined error";
+   * res::Result result = res::Err(err_msg);
+   * assert(result.is_err());
+   * @endcode
+   */
   Err(const std::string& err_msg) : std::string(err_msg) {}
+
   Err(const char* err_msg) : std::string(err_msg) {}
   Err(const ErrStream& err_stream) : std::string(err_stream.str()) {}
 };
--- test/code_test.cpp
+++ test/code_test.cpp
@@ -1,6 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <result/ok.hpp>
 #include <result/result.hpp>
+#include <string>
 
 #undef assert
 #define assert REQUIRE
@@ -9,5 +10,10 @@ TEST_CASE("test code snippet") {
   SECTION("Ok::Ok()") {
     res::Result result = res::Ok();
     assert(result.is_ok());
+  }
+  SECTION("Err::Err(const std::string&)") {
+    std::string err_msg = "undefined error";
+    res::Result result = res::Err(err_msg);
+    assert(result.is_err());
   }
 }
