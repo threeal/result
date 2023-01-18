9b2371a364f8b457f2aadda7c58e552a2701477b
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-18 17:18:39 +0700

    fill API docs for `res::Ok`

--- include/result/ok.hpp
+++ include/result/ok.hpp
@@ -2,7 +2,16 @@
 
 namespace res {
 
+/** A type that represents an ok status (success). */
 struct Ok {
+  /** Construct a new ok data.
+   *
+   * @code{.cpp}
+   * res::Result result = res::Ok();
+   * assert(result.is_ok());
+   * @endcode
+   */
   Ok() {}
 };
+
 }  // namespace res
