5864e03e13e8de910941f9e480bd58b174906400
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-06-26 14:49:59 +0700

    docs: improve the docs of `result::Result` class

--- include/result/result.hpp
+++ include/result/result.hpp
@@ -7,13 +7,15 @@
 
 namespace result {
 
-/** A type used for returning and propagating a value or an error.
- * This type could either have an ok status (success) which contains a value or
- * an error status (failure) which contains an error.
- * is ok.
+/**
+ * @brief Represents a result type that can contain either a value or an error.
  * @tparam T The type of the value.
  *
- * @code
+ * The `Result` class is used to handle operations that can produce either a
+ * value or an error. It provides a way to propagate and handle errors in a
+ * controlled manner.
+ *
+ * @code{.cpp}
  * result::Result<int> res = 32;
  * assert(res.is_ok());
  * assert(res.unwrap() == 32);
@@ -30,45 +32,63 @@ class Result {
   bool data_is_err;
 
  public:
-  /** Construct a new empty result-of.
-   * Defaults to contain an error.
+  /**
+   * @brief Constructs a new empty result.
    *
-   * @code
+   * This constructor creates a new result object that is initialized with an
+   * error by default.
+   *
+   * @code{.cpp}
    * result::Result<int> res;
    * assert(res.is_err());
    * @endcode
    */
-  Result() : Result(error::Error("result-of is uninitialized")) {}
+  Result() : Result(error::Error("result is uninitialized")) {}
 
-  /** Construct a new ok result-of (success) that contains a value.
+  /**
+   * @brief Constructs a new result that contains a value.
    * @param val The value.
    *
-   * @code
+   * @code{.cpp}
    * result::Result<int> res = 32;
    * assert(res.is_ok());
    * @endcode
    */
   Result(const T& val) : data(val), data_is_err(false) {}
 
-  /** Construct a new error result-of (failure).
-   * @param err The error status.
+  /**
+   * @brief Constructs a new result that contains an error.
+   * @param err The error.
    *
-   * @code
+   * @code{.cpp}
    * result::Result<int> res = error::Error("undefined error");
    * assert(res.is_err());
    * @endcode
    */
   Result(const error::Error& err) : data(err), data_is_err(true) {}
 
-  /** Explicitly convert into another result-of with a different value type.
-   * If the status is ok, the value data will be cast into the target value
-   * type. See result::Result::as.
-   * @tparam U the target value type.
+  /**
+   * @brief Explicitly converts the result into another result with a different
+   * value type.
+   * @tparam U The target value type.
    *
-   * @code
+   * This conversion operator allows explicit casting of the result into another
+   * result with a different value type. If the result contains a value, that
+   * value will be explicitly casted into the target value type. If the result
+   * contains an error, copy that error. See also `result::Result::as`.
+   *
+   * @code{.cpp}
    * result::Result<int> int_res = 32;
    * auto float_res = static_cast<result::Result<float>>(int_res);
+   * assert(float_res.is_ok());
    * assert(float_res.unwrap() == 32);
+   * @endcode
+   *
+   * @code{.cpp}
+   * result::Result<int> int_res = error::Error("undefined error");
+   * auto float_res = static_cast<result::Result<float>>(int_res);
+   * assert(float_res.is_err());
+   * assert(float_res.unwrap_err().message == "undefined error");
    * @endcode
    */
   template <typename U>
@@ -77,16 +97,28 @@ class Result {
     return static_cast<U>(std::get<T>(data));
   }
 
-  /** Convert into another result-of with a different value type.
-   * If the status is ok, the value data will be cast into the target value
-   * type.
+  /**
+   * @brief Converts the result into another result with a different value type.
    * @tparam U The target value type.
-   * @return The converted result-of.
+   * @return A converted result of the target type.
    *
-   * @code
+   * This function allows converting the result into another result with a
+   * different value type. If the result contains a value, it will be explicitly
+   * casted into the target value type. If the result contains an error, copy
+   * that error.
+   *
+   * @code{.cpp}
    * result::Result<int> int_res = 32;
    * result::Result<float> float_res = int_res.as<float>();
+   * assert(float_res.is_ok());
    * assert(float_res.unwrap() == 32);
+   * @endcode
+   *
+   * @code{.cpp}
+   * result::Result<int> int_res = error::Error("undefined error");
+   * result::Result<float> float_res = int_res.as<float>();
+   * assert(float_res.is_err());
+   * assert(float_res.unwrap_err().message == "undefined error");
    * @endcode
    */
   template <typename U>
@@ -95,55 +127,65 @@ class Result {
     return static_cast<U>(std::get<T>(data));
   }
 
-  /** Check if the status is ok.
-   * @return `true` if the status is ok.
+  /**
+   * @brief Checks if the result contains a value.
+   * @return `true` if the result contains a value, `false` otherwise.
    */
   bool is_ok() const { return !data_is_err; }
 
-  /** Check if the status is failed.
-   * @return `true` if the status is failed.
+  /**
+   * @brief Checks if the result contains an error.
+   * @return `true` if the result contains an error, `false` otherwise.
    */
   bool is_err() const { return data_is_err; }
 
-  /** Get the value data if the status is ok.
-   * This function will throw an exception if the status is not ok.
-   * @return The value data.
-   * @exception error::Error The status is not ok.
+  /**
+   * @brief Gets the value from the result.
+   * @return A constant reference to the stored value.
+   * @throws error::Error If the result does not contain a value.
    *
-   * @code
+   * This function retrieves the value stored in the result object. If the
+   * result contains a value, it returns a constant reference to the value. If
+   * the result does not contain a value, it throws an exception of type
+   * `error::Error`.
+   *
+   * @code{.cpp}
    * result::Result<int> res = 32;
    * assert(res.unwrap() == 32);
    * @endcode
    *
    * @code{.cpp}
    * result::Result<int> res = error::Error("undefined error");
-   * res.unwrap();  // throws exception
+   * res.unwrap();  // throws error::Error
    * @endcode
    */
   const T& unwrap() const {
-    if (data_is_err)
-      throw error::Error("Unable to unwrap content of error result-of");
+    if (data_is_err) throw error::format("the result contains an error");
     return std::get<T>(data);
   }
 
-  /** Get the error data if the status is failed.
-   * This function will throw an exception if the status is not failed.
-   * @return The error data.
-   * @exception error::Error The status is not failed.
+  /**
+   * @brief Gets the error from the result.
+   * @return A constant reference to the stored error.
+   * @throws error::Error If the result does not contain an error.
    *
-   * @code
+   * This function retrieves the error stored in the result object. If the
+   * result contains an error, it returns a constant reference to the error. If
+   * the result does not contain an error, it throws an exception of type
+   * `error::Error`.
+   *
+   * @code{.cpp}
    * result::Result<int> res = error::Error("undefined error");
    * assert(res.unwrap_err().message == "undefined error");
    * @endcode
    *
    * @code{.cpp}
    * result::Result<int> res = 32;
-   * res.unwrap_err();  // throws exception
+   * res.unwrap_err();  // throws error::Error
    * @endcode
    */
   const error::Error& unwrap_err() const {
-    if (!data_is_err)
-      throw error::Error("Unable to unwrap error of ok result-of");
+    if (!data_is_err) throw error::Error("the result contains a value");
     return std::get<error::Error>(data);
   }
 };
