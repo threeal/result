51d78ede47577240d29bc4aebaa7dcc5270494ca
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-03 13:05:41 +0700

    feat: utilize version 1.0.0 of Errors C++ (#114)

--- test/result_test.cpp
+++ test/result_test.cpp
@@ -6,7 +6,7 @@ TEST_CASE("Result creation") {
   SECTION("Create empty") {
     const result::Result<> res;
     REQUIRE(res.is_err());
-    REQUIRE(res.unwrap_err().message == "the result is uninitialized");
+    REQUIRE(res.unwrap_err().message() == "the result is uninitialized");
   }
 
   SECTION("Create with OK value") {
@@ -21,9 +21,9 @@ TEST_CASE("Result creation") {
   }
 
   SECTION("Create with error") {
-    const result::Result<int> res = error::Error("unknown error");
+    const result::Result<int> res = errors::make("unknown error");
     REQUIRE(res.is_err());
-    REQUIRE(res.unwrap_err().message == "unknown error");
+    REQUIRE(res.unwrap_err().message() == "unknown error");
   }
 }
 
@@ -33,16 +33,16 @@ TEST_CASE("Result data getting") {
 
     SECTION("Get value") { REQUIRE(res.unwrap() == 200); }
 
-    SECTION("Get error") { REQUIRE_THROWS_AS(res.unwrap_err(), error::Error); }
+    SECTION("Get error") { REQUIRE_THROWS_AS(res.unwrap_err(), errors::Error); }
   }
 
   SECTION("From result with error") {
-    const result::Result<int> res = error::Error("unknown error");
+    const result::Result<int> res = errors::make("unknown error");
 
-    SECTION("Get value") { REQUIRE_THROWS_AS(res.unwrap(), error::Error); }
+    SECTION("Get value") { REQUIRE_THROWS_AS(res.unwrap(), errors::Error); }
 
     SECTION("Get error") {
-      REQUIRE(res.unwrap_err().message == "unknown error");
+      REQUIRE(res.unwrap_err().message() == "unknown error");
     }
   }
 }
@@ -54,9 +54,9 @@ TEST_CASE("Result rewriting") {
     REQUIRE(res.unwrap() == 200);
 
     SECTION("Rewrite with error") {
-      res = error::Error("unknown error");
+      res = errors::make("unknown error");
       REQUIRE(res.is_err());
-      REQUIRE(res.unwrap_err().message == "unknown error");
+      REQUIRE(res.unwrap_err().message() == "unknown error");
 
       SECTION("Rewrite again with another value") {
         res = 201;
@@ -67,9 +67,9 @@ TEST_CASE("Result rewriting") {
   }
 
   SECTION("Initialize result with error") {
-    result::Result<int> res = error::Error("unknown error");
+    result::Result<int> res = errors::make("unknown error");
     REQUIRE(res.is_err());
-    REQUIRE(res.unwrap_err().message == "unknown error");
+    REQUIRE(res.unwrap_err().message() == "unknown error");
 
     SECTION("Rewrite with value") {
       res = 200;
@@ -77,9 +77,9 @@ TEST_CASE("Result rewriting") {
       REQUIRE(res.unwrap() == 200);
 
       SECTION("Rewrite again with another error") {
-        res = error::Error("other error");
+        res = errors::make("other error");
         REQUIRE(res.is_err());
-        REQUIRE(res.unwrap_err().message == "other error");
+        REQUIRE(res.unwrap_err().message() == "other error");
       }
     }
   }
@@ -87,7 +87,7 @@ TEST_CASE("Result rewriting") {
 
 TEST_CASE("Result from function getting") {
   const auto square_root = [](double value) -> result::Result<double> {
-    if (value < 0) return error::Error("value must be positive");
+    if (value < 0) return errors::make("value must be positive");
     return std::sqrt(value);
   };
 
@@ -100,7 +100,7 @@ TEST_CASE("Result from function getting") {
   SECTION("From a function returning an error result") {
     const auto res = square_root(-1);
     REQUIRE(res.is_err());
-    REQUIRE(res.unwrap_err().message == "value must be positive");
+    REQUIRE(res.unwrap_err().message() == "value must be positive");
   }
 }
 
@@ -120,7 +120,7 @@ TEST_CASE("Result casting") {
   }
 
   SECTION("Cast result with error") {
-    result::Result<int> src = error::Error("unknown error");
+    result::Result<int> src = errors::make("unknown error");
     result::Result<unsigned int> dst;
 
     SECTION("Using as function") { dst = src.as<unsigned int>(); }
@@ -130,6 +130,6 @@ TEST_CASE("Result casting") {
     }
 
     REQUIRE(dst.is_err());
-    REQUIRE(dst.unwrap_err().message == "unknown error");
+    REQUIRE(dst.unwrap_err().message() == "unknown error");
   }
 }
