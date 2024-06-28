3a80ee125ee50d1fc0561a5584f40cac5b59d3c7
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-28 16:15:20 +0700

    chore: bump CPM.cmake to v0.40.0 (#118)

--- cmake/CPM.cmake
+++ cmake/CPM.cmake
@@ -1,7 +1,7 @@
 file(
   DOWNLOAD
-  https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.39.0/CPM.cmake
+  https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.40.0/CPM.cmake
   ${CMAKE_BINARY_DIR}/_deps/CPM.cmake
-  EXPECTED_MD5 04eefa38baf672f7e8fcd09075122517
+  EXPECTED_MD5 6c9866a0aa0f804a36fe8c3866fb8a2c
 )
 include(${CMAKE_BINARY_DIR}/_deps/CPM.cmake)
